9fce14246a9acb84441c841fe8234ec7b40c126f
GRM Automation <automation@globalreformation.org>
2026-07-19 07:53:22 +0000

    Auto-update videos from YouTube API

--- client/src/lib/siteData.ts
+++ client/src/lib/siteData.ts
@@ -34,9 +34,9 @@ export const pathways = [
 ];
 
 export const videos = [
-  { title: 'La femme selon le cœur de Dieu! 30', age: 'Streamed 5 hours ago', id: 'dh6eVJpVnlY' },
-  { title: 'la femme selon le coeur de Dieu! 30', age: 'Streamed 5 hours ago', id: 'mMW2LkFjwFs' },
-  { title: 'La vie chrétienne en mission! 36', age: 'Streamed 18 hours ago', id: 'JDJrQG6CJGA' },
+  { title: 'La femme selon le cœur de Dieu! 30', age: 'Streamed 11 hours ago', id: 'dh6eVJpVnlY' },
+  { title: 'la femme selon le coeur de Dieu! 30', age: 'Streamed 11 hours ago', id: 'mMW2LkFjwFs' },
+  { title: 'La vie chrétienne en mission! 36', age: 'Streamed 1 day ago', id: 'JDJrQG6CJGA' },
   { title: 'losambo! 10', age: 'Streamed 1 day ago', id: 'f7iRr5v-zqk' },
 ];
 
