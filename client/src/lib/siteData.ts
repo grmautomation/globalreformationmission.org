3eb63b760b516ea1c089b108db90c9473bd7b042
GRM Automation <automation@globalreformation.org>
2026-07-30 19:04:48 +0000

    Auto-update videos from YouTube API

--- client/src/lib/siteData.ts
+++ client/src/lib/siteData.ts
@@ -36,7 +36,7 @@ export const pathways = [
 export const videos = [
   { title: 'La grâce de Dieu qui restaure une famille brisée et prepare la venue du rédempteur! 6', age: 'Streamed 1 day ago', id: 'NfPNhWHZ_Kg' },
   { title: 'La grâce de Dieu qui restaure une famille brisée et prépare la venue du rédempteur! 5', age: 'Streamed 3 days ago', id: 'OxbJRjwHT7c' },
-  { title: 'la femme selon le coeur de Dieu! 31', age: 'Streamed 3 days ago', id: 'fe3AnCQ-NCM' },
+  { title: 'la femme selon le coeur de Dieu! 31', age: 'Streamed 4 days ago', id: 'fe3AnCQ-NCM' },
   { title: 'La femme selon le cœur de Dieu! 31', age: 'Streamed 4 days ago', id: 'nMNftI3b-mo' },
 ];
 
