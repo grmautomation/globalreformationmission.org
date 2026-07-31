2d2c9ed7780cf1865edf5adfc57ad300c3224b69
GRM Automation <automation@globalreformation.org>
2026-07-31 01:24:09 +0000

    Auto-update videos from YouTube API

--- client/src/lib/siteData.ts
+++ client/src/lib/siteData.ts
@@ -34,10 +34,10 @@ export const pathways = [
 ];
 
 export const videos = [
+  { title: 'Global Reformation Mission is live!', age: 'Streamed 4 hours ago', id: 's9YkVvG1fFo' },
+  { title: 'Losambo! 11', age: 'Streamed 5 hours ago', id: '9S27nQw4mLY' },
   { title: 'La grâce de Dieu qui restaure une famille brisée et prepare la venue du rédempteur! 6', age: 'Streamed 1 day ago', id: 'NfPNhWHZ_Kg' },
   { title: 'La grâce de Dieu qui restaure une famille brisée et prépare la venue du rédempteur! 5', age: 'Streamed 3 days ago', id: 'OxbJRjwHT7c' },
-  { title: 'la femme selon le coeur de Dieu! 31', age: 'Streamed 4 days ago', id: 'fe3AnCQ-NCM' },
-  { title: 'La femme selon le cœur de Dieu! 31', age: 'Streamed 4 days ago', id: 'nMNftI3b-mo' },
 ];
 
 export const navItems = [
