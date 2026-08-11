f8c82bec777211c29a49d324ab39e0109788ec4f
GRM Automation <automation@globalreformation.org>
2026-08-11 06:41:47 +0000

    Auto-update videos from YouTube API

--- client/src/lib/siteData.ts
+++ client/src/lib/siteData.ts
@@ -34,9 +34,9 @@ export const pathways = [
 ];
 
 export const videos = [
-  { title: 'Une heure de prière! 1', age: 'Streamed 7 hours ago', id: 'ej6mFdwMVEU' },
-  { title: 'La grâce de Dieu qui restaure une famille brisée et prépare la venue du rédempteur! 9', age: 'Streamed 16 hours ago', id: 'dKhTBmWaHPc' },
-  { title: 'L’Eglise(assemblée)! 20', age: 'Streamed 19 hours ago', id: 'BVaJ-tUf0gU' },
+  { title: 'Une heure de prière! 1', age: 'Streamed 1 hour ago', id: 'ej6mFdwMVEU' },
+  { title: 'La grâce de Dieu qui restaure une famille brisée et prépare la venue du rédempteur! 9', age: 'Streamed 22 hours ago', id: 'dKhTBmWaHPc' },
+  { title: 'L’Eglise(assemblée)! 20', age: 'Streamed 1 day ago', id: 'BVaJ-tUf0gU' },
   { title: 'La femme selon le cœur de Dieu! 33', age: 'Streamed 1 day ago', id: 'dufUwpZ_hpA' },
 ];
 
