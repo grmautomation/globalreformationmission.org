08d6844c99c5dbbce88fa37cd004bd8a9271a1be
GRM Automation <automation@globalreformation.org>
2026-08-12 00:50:56 +0000

    Auto-update videos from YouTube API

--- client/src/lib/siteData.ts
+++ client/src/lib/siteData.ts
@@ -34,10 +34,10 @@ export const pathways = [
 ];
 
 export const videos = [
-  { title: 'Une heure de Prière! 2', age: 'Streamed 1 hour ago', id: '6dOQXA4GbEU' },
-  { title: 'Une heure de prière! 1', age: 'Streamed 13 hours ago', id: 'ej6mFdwMVEU' },
+  { title: 'La Sanctification! 1', age: 'Streamed 4 hours ago', id: 'c-mHoOnDlr8' },
+  { title: 'Une heure de Prière! 2', age: 'Streamed 7 hours ago', id: '6dOQXA4GbEU' },
+  { title: 'Une heure de prière! 1', age: 'Streamed 19 hours ago', id: 'ej6mFdwMVEU' },
   { title: 'La grâce de Dieu qui restaure une famille brisée et prépare la venue du rédempteur! 9', age: 'Streamed 1 day ago', id: 'dKhTBmWaHPc' },
-  { title: 'L’Eglise(assemblée)! 20', age: 'Streamed 1 day ago', id: 'BVaJ-tUf0gU' },
 ];
 
 export const navItems = [
