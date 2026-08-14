f853a974600fa3c6366192eab2ce3ff2cc3f7883
GRM Automation <automation@globalreformation.org>
2026-08-14 07:02:51 +0000

    Auto-update videos from YouTube API

--- client/src/lib/siteData.ts
+++ client/src/lib/siteData.ts
@@ -34,10 +34,10 @@ export const pathways = [
 ];
 
 export const videos = [
-  { title: 'Une heure de prière! 3', age: 'Streamed 19 hours ago', id: 'mkh3mOVTzPc' },
+  { title: 'Une heure de prière! 3', age: 'Streamed 1 day ago', id: 'mkh3mOVTzPc' },
   { title: 'La Sanctification! 1', age: 'Streamed 1 day ago', id: 'c-mHoOnDlr8' },
-  { title: 'Une heure de Prière! 2', age: 'Streamed 1 day ago', id: '6dOQXA4GbEU' },
-  { title: 'Une heure de prière! 1', age: 'Streamed 2 days ago', id: 'ej6mFdwMVEU' },
+  { title: 'Une heure de Prière! 2', age: 'Streamed 2 days ago', id: '6dOQXA4GbEU' },
+  { title: 'Une heure de prière! 1', age: 'Streamed 3 days ago', id: 'ej6mFdwMVEU' },
 ];
 
 export const navItems = [
