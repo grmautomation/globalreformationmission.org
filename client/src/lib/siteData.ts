e7de22e28e074b4a8f809313f815220460fc4708
GRM Automation <automation@globalreformation.org>
2026-08-19 00:28:14 +0000

    Auto-update videos from YouTube API

--- client/src/lib/siteData.ts
+++ client/src/lib/siteData.ts
@@ -34,10 +34,10 @@ export const pathways = [
 ];
 
 export const videos = [
-  { title: 'Global Reformation Mission is live!', age: 'Streamed less than an hour ago', id: '3t_DeQzz5RE' },
+  { title: 'La Sanctification! 3', age: 'Streamed 3 hours ago', id: 'RpIwB8GG7fI' },
+  { title: 'Mardi: intercession! 1', age: 'Streamed 7 hours ago', id: '3t_DeQzz5RE' },
   { title: 'La Santification! 2', age: 'Streamed 1 day ago', id: '-pqq8evMEu8' },
   { title: 'L’Eglise(Assemblée)! 21', age: 'Streamed 1 day ago', id: 'sr4WFnsoJ0s' },
-  { title: 'Global Reformation Mission is live!', age: 'Streamed 2 days ago', id: 'ccYcdI9vgck' },
 ];
 
 export const navItems = [
