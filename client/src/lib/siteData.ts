c9edb92399924fe35e2591bf96ee189485e6ab0b
GRM Automation <automation@globalreformation.org>
2026-09-02 20:10:39 +0000

    Auto-update videos from YouTube API

--- client/src/lib/siteData.ts
+++ client/src/lib/siteData.ts
@@ -34,10 +34,10 @@ export const pathways = [
 ];
 
 export const videos = [
-  { title: 'La Sanctification! 5', age: 'Streamed 6 hours ago', id: '-yoGuR0Wg5c' },
+  { title: '2 Thessaloniciens! 7', age: 'Streamed 2 hours ago', id: 'diIEdqcLMsc' },
+  { title: 'La Sanctification! 5', age: 'Streamed 10 hours ago', id: '-yoGuR0Wg5c' },
   { title: 'La femme selon le cœur de Dieu! 36', age: 'Streamed 3 days ago', id: '3vED10_GGM0' },
   { title: '2 Timothée! 4', age: 'Streamed 3 days ago', id: 'FcTnVjOg26U' },
-  { title: '2 Thessaloniciens! 6', age: 'Streamed 4 days ago', id: '6hnc_AJTwmg' },
 ];
 
 export const navItems = [
