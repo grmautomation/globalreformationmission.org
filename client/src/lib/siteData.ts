2b64380878a1ba665122b27357b665689e0c764e
GRM Automation <automation@globalreformation.org>
2026-09-21 11:13:02 +0000

    Auto-update videos from YouTube API

--- client/src/lib/siteData.ts
+++ client/src/lib/siteData.ts
@@ -34,10 +34,10 @@ export const pathways = [
 ];
 
 export const videos = [
-  { title: 'La Sanctification! 9', age: 'Streamed 5 hours ago', id: 'LC_hhp7uGzs' },
-  { title: 'Why did Jesus give Simon the name Cephas?', age: 'Streamed 9 hours ago', id: 'PAHlNYwW9Js' },
-  { title: 'La Sanctification! 8', age: 'Streamed 4 days ago', id: 'z-a3uyG8h0c' },
-  { title: 'La sanctification! 7', age: 'Streamed 6 days ago', id: 'HT8MKQ9_8h8' },
+  { title: 'La Sanctification! 9', age: 'Streamed 2 hours ago', id: 'LC_hhp7uGzs' },
+  { title: 'Why did Jesus give Simon the name Cephas?', age: 'Streamed 6 hours ago', id: 'PAHlNYwW9Js' },
+  { title: 'La Sanctification! 8', age: 'Streamed 5 days ago', id: 'z-a3uyG8h0c' },
+  { title: 'La sanctification! 7', age: 'Streamed 1 week ago', id: 'HT8MKQ9_8h8' },
 ];
 
 export const navItems = [
