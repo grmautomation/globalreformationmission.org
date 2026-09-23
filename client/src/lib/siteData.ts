d9af04783e0b6c609050632b228e644fdd858572
GRM Automation <automation@globalreformation.org>
2026-09-23 01:59:50 +0000

    Auto-update videos from YouTube API

--- client/src/lib/siteData.ts
+++ client/src/lib/siteData.ts
@@ -34,8 +34,8 @@ export const pathways = [
 ];
 
 export const videos = [
-  { title: 'La Sanctification! 10', age: 'Streamed less than an hour ago', id: 'Q1-3TNj7U2o' },
-  { title: 'Discipleship meeting! 1', age: 'Streamed 9 hours ago', id: 'MIVpQ5-qdII' },
+  { title: 'La Sanctification! 10', age: 'Streamed 5 hours ago', id: 'Q1-3TNj7U2o' },
+  { title: 'Discipleship meeting! 1', age: 'Streamed 15 hours ago', id: 'MIVpQ5-qdII' },
   { title: 'La Sanctification! 9', age: 'Streamed 1 day ago', id: 'LC_hhp7uGzs' },
   { title: 'Why did Jesus give Simon the name Cephas?', age: 'Streamed 1 day ago', id: 'PAHlNYwW9Js' },
 ];
